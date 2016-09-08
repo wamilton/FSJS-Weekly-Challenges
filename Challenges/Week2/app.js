d5c1b6f6b44429f460d13c7c803713e32facb68a
Walter Milt <walter.milton@gmail.com>
2016-09-07 20:15:01 -0400

    completed challege

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -6,3 +6,21 @@
 //Call function
 
 //Alert user results
+
+
+function processMessage(msg)
+{
+console.log(msg);
+}
+if (msg === 'Hello') {
+  alert('Hello World');
+}
+else {
+  alert('You did not say hello.');
+}
+
+var message;
+
+message = prompt();
+
+processMessage(Message);
